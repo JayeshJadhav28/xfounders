9e06badaeedb0776c9506049a2e0ee3eb8a65a00
JayeshJadhav28 <jayeshjadhav6480@gmail.com>
2025-08-11 11:42:41 +0530

    chore(firebase): read config from NEXT_PUBLIC_* env vars

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,15 +3,16 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 import { getAnalytics } from "firebase/analytics"
 
+// Read Firebase config from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBeS-3BZ8kv8-JDCeda3eoDLEs5RHPC7AM",
-  authDomain: "xfounders-ecelldiet.firebaseapp.com",
-  projectId: "xfounders-ecelldiet",
-  storageBucket: "xfounders-ecelldiet.firebasestorage.app",
-  messagingSenderId: "976500840296",
-  appId: "1:976500840296:web:73bab96b77d6b8712568dd",
-  measurementId: "G-L7CJ8ZBFEY",
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+} as const
 
 const app = initializeApp(firebaseConfig)
 
